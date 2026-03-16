53fb02076b5d159785e00c06f27a3bb98aa73752
Vasishtha Birari <birarivasishtha@gmail.com>
2026-03-17 03:07:26 +0530

    some error

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 
+const base = process.env.VITE_BASE_PATH || '/'
+
 export default defineConfig({
-  base: "/db/",
+  base,
   plugins: [react()]
 })
